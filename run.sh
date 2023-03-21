91504ced1663cc7bb1bfb9bd31e6fd412fe213f8
arn-all <arnaud.allera@cea.fr>
2023-03-21 14:04:45 +0100

    shell mode

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-# Pass all command-line arguments to mpirun and append the MILADY executable path
-exec mpirun "$@" /MLD/mld_build_intel/bin/milady_main.exe
+if [[ "$1" != "shell" ]]; then
+	# Pass all command-line arguments to mpirun and append the MILADY executable path
+	exec mpirun "$@" /MLD/mld_build_intel/bin/milady_main.exe
+else
+	# start a terminal
+	/bin/bash 
+fi
